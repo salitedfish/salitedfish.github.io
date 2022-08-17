5283c910e032f95a45ca96d57e8504fde3c24cdb
salitedfish <1525185228@qq.com>
2022-08-17 16:03:00 +0800

    chore: init

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -1,7 +1,41 @@
-import { defineUserConfig } from "vuepress"
+import { defineUserConfig, defaultTheme } from "vuepress"
+import { searchPlugin } from "@vuepress/plugin-search"
 
 export default defineUserConfig({
   lang: "zh-CN",
-  title: "你好， VuePress ！",
-  description: "这是我的第一个 VuePress 站点",
+  title: "Good-Look",
+  description: "",
+  head: [["link", { rel: "icon", href: "/img/favicon.ico" }]],
+  theme: defaultTheme({
+    logo: "/img/logo.jpg",
+    navbar: [],
+    sidebar: [
+      {
+        text: "javascript",
+        collapsible: true,
+        children: ["/javascript/事件循环.md"],
+      },
+      {
+        text: "typescript",
+        collapsible: true,
+        children: ["/typescript/内置工具类型.md"],
+      },
+      {
+        text: "node",
+        collapsible: true,
+        children: ["/node/内置模块.md"],
+      },
+      {
+        text: "vue",
+        collapsible: true,
+        children: ["/vue/响应式原理.md"],
+      },
+      {
+        text: "react",
+        collapsible: true,
+        children: [],
+      },
+    ],
+  }),
+  plugins: [searchPlugin({})],
 })
